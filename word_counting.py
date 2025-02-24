ee0b5304924bcb8befe0d919e47c8c823641fd5d
vadimastashin <67231182+vadimastashin@users.noreply.github.com>
2025-02-24 16:38:47 +0300

    Add files via upload

---
+++ word_counting.py
@@ -0,0 +1,13 @@
+#line = 'this is a string with a few words'
+#total_words = line.count(' ') + 1
+#print(total_words)
+# или
+#print('this is a string with a few words'.count(' ') + 1)
+
+line = input()
+total_words = line.count(' ') + 1
+print(total_words)
+
+
+
+
