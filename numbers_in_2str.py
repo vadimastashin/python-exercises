388f9d5ef9cbdcb87be61839ce16f1bea92c3742
vadimastashin <67231182+vadimastashin@users.noreply.github.com>
2025-02-26 15:40:32 +0300

    Add files via upload

---
+++ numbers_in_2str.py
@@ -0,0 +1,9 @@
+"""
+Условие
+Напишите программу, которая считывает целое число и выводит текст,
+аналогичный приведенному в примере (пробелы важны!).
+"""
+
+num = int(input())
+print("The next number for the number " + str(num) + " is " + str(num + 1) + ".")
+print("The previous number for the number " + str(num) + " is " + str(num - 1) + ".")
